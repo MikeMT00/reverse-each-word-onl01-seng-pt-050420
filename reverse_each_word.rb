0e751913a90f46d0221bb17e2ff3d9e856f9a74b
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-12 16:04:45 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(word)
+    word.each do
+    puts "Hello there, and how are you?"
+  end
+end
+
+
+
+
+
+
+#def hamburger(toppings)
+#  toppings.each do |topping|
+#    puts "I love #{topping} on my burgers!"
+#  end
+#end
